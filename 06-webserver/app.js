a56441ad2cc22a785df6af45a1c5fd09a6ffd633
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-03-12 22:59:40 -0500

    [update] Métodos http.resp.write y http.resp.setHeader, y demás

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,7 +1,20 @@
 const http = require("http");
 http
   .createServer((req, resp) => {
-    resp.write("Hola mundo");
+    // resp.writeHead(200, { "Content-Type": "application/json" });
+    resp.setHeader("Content-Disposition", "attachment; filename=lista.csv");
+    resp.writeHead(200, { "Content-Type": "application/csv" });
+
+    // const persona = {
+    //   id: 1,
+    //   nombre: "Pierre",
+    // };
+
+    // resp.write(JSON.stringify(persona));
+    resp.write("id, nombre\n");
+    resp.write("1, nombre1\n");
+    resp.write("2, nombre2\n");
+    resp.write("3, nombre3\n");
     resp.end();
   })
   .listen(8080);
